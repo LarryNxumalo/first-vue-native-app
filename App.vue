7f61a9bfeaa7d619d335a2819fd456deab35cff6
Larry Nxumalo <larry.nxumalo@afrihost.com>
2020-08-10 23:06:16 +0200

    vue natively

---
+++ App.vue
@@ -0,0 +1,15 @@
+<template>
+    <text-input
+        :style="{height: 40, width: 100, borderColor: 'gray', borderWidth: 1}"
+        v-model="text"
+      />
+</template>
+<script>
+    export default {
+        data: function() {
+            return {
+                text: ''
+            };
+        }
+    }
+</script>
